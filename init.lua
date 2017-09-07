416b6e1f25aaf58cb7ec64cb86561fba712b2cfe
Nan <ouchxp.me@gmail.com>
2017-09-07 17:00:33 +1200

    Add application shortcut

--- init.lua
+++ init.lua
@@ -6,3 +6,8 @@ require "window"
 -----------------------------------------------
 hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
 hs.alert.show("Hammerspoon config reloaded")
+
+local alt = {"alt"}
+hs.hotkey.bind(alt,    "1",     function() hs.application.open("iTerm") end)
+hs.hotkey.bind(alt,    "2",     function() hs.application.open("Emacs") end)
+hs.hotkey.bind(alt,    "3",     function() hs.application.open("Google Chrome") end)
