b2dabfb76405b0727cdb8ea82810c4c3f59a9aca
Nan Wu <nan@regrow.ag>
2025-11-20 10:51:32 +1300

    update vscode package name

--- shortcut.lua
+++ shortcut.lua
@@ -1,6 +1,6 @@
 local alt = {"alt"}
 hs.hotkey.bind(alt,    "1",     function() hs.application.open("com.googlecode.iterm2") end)
 hs.hotkey.bind(alt,    "2",     function() hs.application.open("com.jetbrains.intellij") end)
-hs.hotkey.bind(alt,    "3",     function() hs.application.open("org.microsoft.VSCode") end)
+hs.hotkey.bind(alt,    "3",     function() hs.application.open("com.microsoft.VSCode") end)
 hs.hotkey.bind(alt,    "4",     function() hs.application.open("org.mozilla.firefox") end)
 hs.hotkey.bind(alt,    "5",     function() hs.application.open("com.google.Chrome") end)
